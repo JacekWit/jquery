a564b43ef43ea62c246dcf0f52a769c5d2d6a94e
JacekWit <jacek_witkowski@interia.pl>
2017-06-24 15:26:04 +0200

    init

---
+++ js/4-find-each.js
@@ -0,0 +1,16 @@
+"use strict";
+
+//Funckja document.ready() - wersja skrócona
+$(function) {
+
+//Wybieranie elementu z kolekcji za pomocą metody eq()
+$('body').find('p').eq(1).css({'color': 'green'});
+
+
+//Pętla each dodająca do paragrafów klasę z kolejnym numerem
+$('p').each(function (index) {
+        $(this).addClass('paragraf-') + index); 
+    
+    console.log(jQuery(this));
+});
+});
